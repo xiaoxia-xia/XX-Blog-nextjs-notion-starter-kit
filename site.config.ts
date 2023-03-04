5e950af393a2810348dd66e4b332a7ad2dfe39a4
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2023-03-04 15:22:43 -0800

    test

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8cea8c18a3ca418481d7c1248de9be09',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
