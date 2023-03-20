42af15990a1e8b160426423a81cc3c3675004d55
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2023-03-20 00:41:31 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Xiaoxia Xia',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'This Is XIAOXIA Website',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
