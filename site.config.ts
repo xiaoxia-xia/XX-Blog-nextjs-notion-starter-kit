40be899cc5c89110720904638b458426751f0189
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2023-03-20 00:35:36 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Xiaoxia Personal Website',
+  domain: 'https://xiaoxia-blog.vercel.app/',
+  author: 'Xiaoxia Xia',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
